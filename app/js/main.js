1e37931f48b69847ae03434e228c3bade63063ec
veS-Sev <newsecretword@yandex.ru>
2020-05-11 19:29:49 +0300

    from header to reviews(not complete)

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
-$(function(){
+$(function () {
 
 
-
-  
+  $('.slider__inner').slick({
+    infinite: true,
+    autoplay: true,
+    speed: 600,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow:'.slider__arrow-left',
+    nextArrow:'.slider__arrow-right'
+      });
 });
