d27030392c0c05e53dbeefa8bcb4a2fbc941e14c
veS-Sev <newsecretword@yandex.ru>
2020-05-14 20:19:09 +0300

    contacts and footer added

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,21 @@ $(function () {
     nextArrow:'.slider__arrow-right'
       });
 
+      $('.reviews__items').slick({
+        arrows:false,
+        infinite: true,
+        autoplay: true,
+        speed: 900,
+        fade: true,
+        cssEase: 'linear',
+        dots:true,
+        dotsClass:'reviews__slick-dots',
+
+        // appendDots:$('.reviews__items-dots')
+        // prevArrow:'.slider__arrow-left',
+        // nextArrow:'.slider__arrow-right'
+          });
+
       $('.header__basket-icon').on('click', function () {
         $('.basket__inner').toggleClass('active');
       });
