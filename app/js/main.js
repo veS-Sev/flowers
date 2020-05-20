96f200f48c4357e63304b677e2fdcf529d45ff97
veS-Sev <newsecretword@yandex.ru>
2020-05-20 20:34:16 +0300

    fixed. not complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,5 @@
 $(function () {
-
-
-  $('.slider__inner').slick({
-    infinite: true,
-    autoplay: true,
-    speed: 600,
-    fade: true,
-    cssEase: 'linear',
-    prevArrow:'.slider__arrow-left',
-    nextArrow:'.slider__arrow-right'
-      });
-
+     
       $('.reviews__items').slick({
         arrows:false,
         infinite: true,
@@ -20,15 +9,26 @@ $(function () {
         cssEase: 'linear',
         dots:true,
         dotsClass:'reviews__slick-dots',
-
-        // appendDots:$('.reviews__items-dots')
-        // prevArrow:'.slider__arrow-left',
-        // nextArrow:'.slider__arrow-right'
           });
 
       $('.header__basket-icon').on('click', function () {
         $('.basket__inner').toggleClass('active');
       });
+     
+      new WOW().init();
 
 
-});
+    });
+    
+    let $layers = $('div[data-role="layer"]');
+  
+$(window).on('scroll', function(e) {
+   
+  $layers.each(function(i) {
+    let $layer = $(this),
+        coef = $layer.data('coef'),
+        scroll = $(window).scrollTop();
+         
+    $layer.css('top' , (scroll * coef) + 'px');
+  });
+   });
--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ let gulp = require('gulp'),
       'node_modules/slick-carousel/slick/slick.css',
       'node_modules/magnific-popup/dist/magnific-popup.css',
       'node_modules/normalize.css/normalize.css',
+      'node_modules/animate.css/animate.css',
       ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -35,6 +36,7 @@ let gulp = require('gulp'),
       return gulp.src([
       'node_modules/slick-carousel/slick/slick.js',
       'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+      'node_modules/wow.js/dist/wow.js'
       ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
